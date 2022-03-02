3fe9a83f45035d27568fccc9ca786f3cf533e2e8
DendeSon <1341511+DendeSon@users.noreply.github.com>
2022-03-02 01:15:05 -0600

    Added problems
    Added problem set 1, problems 1 thru 3

---
+++ Set1Problem2.py
@@ -0,0 +1,14 @@
+"""
+Objective: Find how many times 'bob' appears in a string, 's'
+'s' is given by the grader
+Prints out how many times 'bob' was found in a string, 's'
+"""
+
+b = "bob"
+count = 0
+
+for word in range(len(s)):
+    if s[word:word + 3] == b:
+        count += 1
+
+print("Number of times bob occurs is: " + str(count))
