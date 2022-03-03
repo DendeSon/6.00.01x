9ef5eee49d996a859156c23bf1eca5371421ea6d
DendeSon <1341511+DendeSon@users.noreply.github.com>
2022-03-03 14:40:47 -0600

    Create Set2Problem3.py

---
+++ Set2Problem3.py
@@ -0,0 +1,25 @@
+"""
+Objective: Find a minimum fixed monthly payment needed to pay off credit card
+within 12 months using bisection search.
+'balance' and 'annualInterestRate' are given by the grader.
+Prints out the lowest fixed monthly payment rate, rounded to 2 decimals.
+"""
+
+init_balance = balance
+monthlyInterestRate = annualInterestRate/12
+lower = init_balance/12
+upper = (init_balance * (1 + monthlyInterestRate)**12)/12.0
+epsilon = 0.03
+
+while abs(balance) > epsilon:
+    monthlyPaymentRate = (upper + lower)/2
+    balance = init_balance
+    for i in range(12):
+        balance = balance - monthlyPaymentRate + ((balance - monthlyPaymentRate) * monthlyInterestRate)
+    if balance > epsilon:
+        lower = monthlyPaymentRate
+    elif balance < -epsilon:
+        upper = monthlyPaymentRate
+    else:
+        break
+print('Lowest Payment:', round(monthlyPaymentRate, 2))
